fb5feab2636b415eaa1b1e0cdbd4458180560a83
Melvin Sinatra <melvin.sinatra2002@gmail.com>
2022-11-07 12:15:28 +0700

    change BrowserRouter to HahsRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { HomePage, AboutMe, MyPortfolio, ContactMe } from './pages';
 import { useContext } from 'react';
 import { ThemeContext, ThemeModeContext } from './contexts/ThemeContext';
@@ -7,22 +7,22 @@ import { ThemeButton, Navbar } from './components';
 import MobileHomePage from './pages/mobile-home-page';
 
 function App() {
-	const theme = useContext(ThemeContext); 
+	const theme = useContext(ThemeContext);
 	const isMobile = window.innerWidth < theme?.breakpoints.values.md;
 
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<div className="App" style={{ backgroundColor: theme.palette.background.default, color: theme.palette.text.primary }}>
 				<ThemeButton />
 				<Navbar />
 				<Routes>
-					<Route path="/" element={isMobile ? <MobileHomePage/> : <HomePage />} />
+					<Route path="/" element={isMobile ? <MobileHomePage /> : <HomePage />} />
 					<Route path="/about-me" element={<AboutMe />} />
 					<Route path="/my-portfolio" element={<MyPortfolio />} />
 					<Route path="/contact-me" element={<ContactMe />} />
 				</Routes>
 			</div>
-		</BrowserRouter>
+		</HashRouter>
 	);
 }
 
