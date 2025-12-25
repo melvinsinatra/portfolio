31b9d075169a8f602db527fced58910d9864e486
Melvin Sinatra <melvin.sinatra2002@gmail.com>
2025-12-25 22:10:52 +0700

    Add gsap and implement simple scroll trigger animation.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script>
-  import '../app.css';
+  import { onMount } from "svelte";
+  import { gsap } from "gsap";
+  import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+  import "../app.css";
   import data from "$lib/data/profile.json";
   import Hero from "$lib/components/Hero.svelte";
   import About from "$lib/components/About.svelte";
@@ -8,14 +12,74 @@
   import Skills from "$lib/components/Skills.svelte";
   import Services from "$lib/components/Services.svelte";
   import Footer from "$lib/components/Footer.svelte";
+
+  let hero;
+  let about;
+  let skills;
+  let services;
+  let experience;
+  let projects;
+  let footer;
+
+  onMount(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const sections = [
+      hero,
+      about,
+      skills,
+      services,
+      experience,
+      projects,
+      footer
+    ];
+
+    sections.forEach((section) => {
+      if (!section) return;
+
+      gsap.from(section, {
+        opacity: 0,
+        y: 40,
+        duration: 0.6,
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: section,
+          start: "top 85%",
+          toggleActions: "play none none none"
+        }
+      });
+    });
+
+    return () => ScrollTrigger.getAll().forEach(t => t.kill());
+  });
 </script>
 
 <main class="max-w-6xl mx-auto px-6 space-y-32">
-  <Hero {data} />
-  <About {data} />
-  <Skills {data} />
-  <Services {data} />
-  <Experience {data} />
-  <Projects {data} />
-  <Footer {data} />
+  <div bind:this={hero}>
+    <Hero {data} />
+  </div>
+
+  <div bind:this={about}>
+    <About {data} />
+  </div>
+
+  <div bind:this={skills}>
+    <Skills {data} />
+  </div>
+
+  <div bind:this={services}>
+    <Services {data} />
+  </div>
+
+  <div bind:this={experience}>
+    <Experience {data} />
+  </div>
+
+  <div bind:this={projects}>
+    <Projects {data} />
+  </div>
+
+  <div bind:this={footer}>
+    <Footer {data} />
+  </div>
 </main>
